c71238516f8e27e17a62643b1ada8ddaeedcf52b
Geordie Pearson <geordiepearson2000@outlook.com>
2020-01-11 04:03:44 +1000

    Game fully functional

--- tictactoe.c
+++ tictactoe.c
@@ -6,7 +6,8 @@ void printBoard(int gameBoard[]);
 void playTurn(int gameBoard[], int* turn);
 int checkInt(int* index);
 int isdigit(int arg);
-void hasWon(int gameBoard[]);
+void hasWon(int gameBoard[], bool* gameOver);
+void resetGame(int gameBoard[], int*turn, bool* gameOver);
 
 int main(int argc, char** argv){
 	bool gameOver = false; 
@@ -15,9 +16,16 @@ int main(int argc, char** argv){
 	
 	printf("Hello, Welcome to Tic Tac Toe.\n");
 
-	printBoard(gameBoard);
-	hasWon(gameBoard);
-
+	while(1){
+		printBoard(gameBoard);
+		playTurn(gameBoard, &turn);
+		hasWon(gameBoard, &gameOver);
+		if(gameOver)
+		{
+			resetGame(gameBoard, &turn, &gameOver);
+		}
+	}
+	
 	return 0;
 }
 
@@ -45,16 +53,21 @@ void playTurn(int gameBoard[], int* turn){
 	}
 	
 	index = input-1;
-
-	if (*turn % 2 == 0){
-		gameBoard[index] = 2;
+	if(gameBoard[index] != 0){
+		printf("Square already taken. \n");
 	}
 
 	else{
-		gameBoard[index] = 1;;
-	}
+		if (*turn % 2 == 0){
+			gameBoard[index] = 2;
+		}
 
-	++ *turn;
+		else{
+			gameBoard[index] = 1;;
+		}
+
+		++ *turn;
+	}
 }
 
 int checkInt(int* input){
@@ -64,17 +77,57 @@ int checkInt(int* input){
 	if(isdigit(intChecker)!=0){
 			*input = intChecker - '0';
 			return 1;
-			}
+	}
 
 	else{
 		return 0;
-		}
+	}
 }
 
-void hasWon(int gameBoard[]){
-	for (int i = 0; i < 7; i += 3){
-		if(gameBoard[i] == gameBoard[i+1] == gameBoard[i+2]){
-			printf("winner \n");
+void hasWon(int gameBoard[], bool* gameOver){
+	for (int i = 0; i < 7; i += 3){ /*checks rows*/
+		if(gameBoard[i] == gameBoard[i+1] && gameBoard[i+1]== gameBoard[i+2]){
+			if(gameBoard[i] != 0){
+			*gameOver = true;
+			}	
 		}
+	}
+
+	for (int i = 0; i < 3; ++i){/*checks columns*/
+		if(gameBoard[i] == gameBoard[i+3] && gameBoard[i+3] == gameBoard[i+6]){
+			if(gameBoard[i] != 0){
+			*gameOver = true;
+			}	
+		}
+	}
+
+	if(gameBoard[0] == gameBoard[4] && gameBoard[4] == gameBoard[8]){/*check diagonal 1*/
+		if(gameBoard[4] != 0){
+			*gameOver = true;
+		}	
+	}
+
+	if(gameBoard[2] == gameBoard[4] && gameBoard[4] == gameBoard[6]){/*checks diagonal 2*/
+		if(gameBoard[4] != 0){
+			*gameOver = true;
+		}	
 	}	
+}
+
+void resetGame(int gameBoard[], int* turn, bool* gameOver){
+	int i;
+
+	if (*turn % 2 == 0)
+	{
+		printf("Player 1 Wins. Starting New Game. \n\n");
+	}
+	else{
+		printf("Player 2 Wins. \n");
+	}
+
+	*gameOver = false; 
+	*turn = 1;
+	for (int i = 0; i < 9; ++i){
+		gameBoard[i] = 0;
+	}
 }
